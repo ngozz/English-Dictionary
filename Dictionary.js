25a7b19729cf58aa80dc5258a923c410173889fc
ngozz <hieungoanngoan@gmail.com>
2022-06-23 13:05:01 +0700

    API console test

--- Dictionary.js
+++ Dictionary.js
@@ -1,1 +1,21 @@
-console.log("Hello")
+console.log("Hello");
+
+let input = document.querySelector(".input");
+let searchBtn = document.querySelector(".button");
+let defBox = document.querySelector(".def");
+
+searchBtn.addEventListener("click", function(e){
+  e.preventDefault();
+  let word = input.value;
+  if (word === "") {
+    alert("Word required");
+    return;
+  }
+  getData(word);
+});
+
+async function getData(word){
+  const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+  const data = await response.json();
+  console.log(data);
+}
