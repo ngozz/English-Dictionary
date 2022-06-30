c8d8ebb4f25c8fbf95c3e0bfc6ffe3a15dd3ee5e
ngozz <hieungoanngoan@gmail.com>
2022-07-01 00:54:36 +0700

    Added Log in

---
+++ js/Login.js
@@ -0,0 +1,13 @@
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let searchBtn = document.querySelector(".button");
+
+
+searchBtn.addEventListener("click", function(e){
+    e.preventDefault();
+  
+    if (username.value == "admin" && password.value == "admin"){
+        alert ("Login successfully");
+        window.location = "../ENG-VIE.html"
+    }
+});
